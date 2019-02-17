aa7bd93f8e3525e0b961007e5f0e127d5a8939bf
Abubakre Abdulqudus <abdulqudusabubakre@gmail.com>
2019-02-17 09:33:19 +0100

    Completed js calc function

--- my-work/number5/calculate.js
+++ my-work/number5/calculate.js
@@ -1,17 +1,17 @@
 function performOperation(id, nums) {
   let result;
   switch (id) {
-    case 'add':
-      result = parseFloat(nums[0]) + parseFloat(nums[1])
+    case "add":
+      result = parseFloat(nums[0]) + parseFloat(nums[1]);
       break;
-    case 'subtract':
-      result = parseFloat(nums[0]) - parseFloat(nums[1])
+    case "subtract":
+      result = parseFloat(nums[0]) - parseFloat(nums[1]);
       break;
-    case 'divide':
-      result = parseFloat(nums[0]) / parseFloat(nums[1])
+    case "divide":
+      result = parseFloat(nums[0]) / parseFloat(nums[1]);
       break;
-    case 'product':
-      result = parseFloat(nums[0]) * parseFloat(nums[1])
+    case "product":
+      result = parseFloat(nums[0]) * parseFloat(nums[1]);
       break;
     default:
       break;
@@ -19,71 +19,61 @@ function performOperation(id, nums) {
   return result;
 }
 
-window.addEventListener('load', () => {
-  let previousText = document.getElementById('previous');
-  let currentText = document.getElementById('current');
+window.addEventListener("load", () => {
+  let previousText = document.getElementById("previous");
+  let currentText = document.getElementById("current");
   // Initializing buttons
-  let clear = document.getElementById('clear');
-  let numbers = document.querySelectorAll('.grid-item');
-  let signs = document.querySelectorAll('.grid-item.last');
-  let add = document.getElementById('add');
-  let subtract = document.getElementById('subtract');
-  let divide = document.getElementById('divide');
-  let product = document.getElementById('product');
-  let equals = document.getElementById('equals');
-  let percentage = document.getElementById('percentage');
+  let clear = document.getElementById("clear");
+  let numbers = document.querySelectorAll(".calc-btns");
+  let signs = document.querySelectorAll(".calc-btns.last");
+  let add = document.getElementById("add");
+  let subtract = document.getElementById("subtract");
+  let divide = document.getElementById("divide");
+  let product = document.getElementById("product");
+  let equals = document.getElementById("equals");
+  let percentage = document.getElementById("percentage");
 
   let nums = [];
-  let numberToBeAdded = '';
-  let result = '';
-  let lastSign = '';
-
-  signs.forEach(sign => {
-    sign.addEventListener('click', () => {
-      if (sign.id != 'equals') {
-        if (nums.length > 1) {
-          nums = [];
-          nums.push(result);
-        }
-        lastSign = sign.id;
-        numberToBeAdded = '';
-      }
-    });
-  });
+  let numberToBeAdded = "";
+  let result = "";
+  let lastSign = "";
 
   // Modifiy content of screen on number press
-  let typedNumber = '';
+  let typedNumber = "";
   numbers.forEach(number => {
-    number.addEventListener('click', () => {
-      if (number.id != 'equals') {
+    number.addEventListener("click", () => {
+      if (number.id != "equals") {
         typedNumber += number.innerHTML;
         currentText.innerHTML = typedNumber;
-        if (number.classList.value.indexOf('last') < 0) {
-          numberToBeAdded += number.innerHTML;
-          nums.push(numberToBeAdded);
-          if (nums.length > 1) {
-            result = performOperation(lastSign, nums);
-          }
-        }
       }
     });
   });
 
-  equals.addEventListener('click', () => {
-    let finalResult = performOperation(lastSign, nums);
+  equals.addEventListener("click", () => {
+    let newNum = typedNumber;
+    if (typedNumber.split("").indexOf("x") > -1) {
+      let index = typedNumber.split("").indexOf("x");
+      newNum = typedNumber.replace("x", "*");
+    }
+    let dividedBy = String.fromCharCode("0xf7");
+    if (typedNumber.split("").indexOf(dividedBy) > -1) {
+      let index = typedNumber.split("").indexOf(dividedBy);
+      newNum = typedNumber.replace(dividedBy, "/");
+    }
+    let finalResult = eval(newNum);
     previousText.innerHTML = typedNumber;
     typedNumber = finalResult;
-    nums = [finalResult];
+    // nums = [finalResult];
     currentText.innerHTML = finalResult;
-    numberToBeAdded = finalResult;
+    // numberToBeAdded = finalResult;
   });
 
   // Clear screen on 'C' btn press
-  clear.addEventListener('click', () => {
-    previousText.innerHTML = '0';
-    currentText.innerHTML = '0';
-    typedNumber = '';
+  clear.addEventListener("click", () => {
+    previousText.innerHTML = "0";
+    currentText.innerHTML = "0";
+    typedNumber = "";
     nums = [];
-    numberToBeAdded = '';
-  })
+    numberToBeAdded = "";
+  });
 });
