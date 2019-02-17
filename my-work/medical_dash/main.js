c771540b7ccafdd586540fab94bedf02d3a1467f
Abubakre Abdulqudus <abdulqudusabubakre@gmail.com>
2019-02-17 13:48:45 +0100

    Added side menu items

---
+++ my-work/medical_dash/main.js
@@ -0,0 +1,11 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "80vw";
+  document.getElementById('sidenav-sidemenu').style.width = '20%';
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById('sidenav-sidemenu').style.width = '0';
+}
