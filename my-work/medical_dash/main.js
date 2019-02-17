920fb3ba91f66982359e5d50cc452ede1de8be94
Abubakre Abdulqudus <abdulqudusabubakre@gmail.com>
2019-02-17 18:01:15 +0100

    Updated sidebar

--- my-work/medical_dash/main.js
+++ my-work/medical_dash/main.js
@@ -2,10 +2,12 @@
 function openNav() {
   document.getElementById("mySidenav").style.width = "80vw";
   document.getElementById('sidenav-sidemenu').style.width = '20%';
+  document.getElementById('sidenav-content').style.width = '80%';
 }
 
 /* Set the width of the side navigation to 0 */
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById('sidenav-sidemenu').style.width = '0';
+  document.getElementById('sidenav-content').style.width = '0';
 }
