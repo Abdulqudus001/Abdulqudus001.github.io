b348a198adf2fe948092313030083dc0e2e7196e
Abubakre Abdulqudus <abdulqudusabubakre@gmail.com>
2019-02-18 13:03:23 +0100

    Added screen toggle function

--- my-work/medical_dash/main.js
+++ my-work/medical_dash/main.js
@@ -1,27 +1,35 @@
 /* Set the width of the side navigation to 250px */
 function openNav() {
   document.getElementById("mySidenav").style.width = "80vw";
-  document.getElementById('sidenav-sidemenu').style.width = '20%';
-  document.getElementById('sidenav-content').style.width = '80%';
+  document.getElementById("sidenav-sidemenu").style.width = "20%";
+  document.getElementById("sidenav-content").style.width = "80%";
 }
 
 /* Set the width of the side navigation to 0 */
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
-  document.getElementById('sidenav-sidemenu').style.width = '0';
-  document.getElementById('sidenav-content').style.width = '0';
+  document.getElementById("sidenav-sidemenu").style.width = "0";
+  document.getElementById("sidenav-content").style.width = "0";
 }
 
-let sidebarLinks = document.querySelectorAll('.selected-navmenu');
-let lines = document.querySelectorAll('.hidden-line');
+let sidebarLinks = document.querySelectorAll(".selected-navmenu");
+let lines = document.querySelectorAll(".hidden-line");
+let screens = document.querySelectorAll(".screens");
 
 sidebarLinks.forEach(link => {
-  link.addEventListener('click', () => {
+  link.addEventListener("click", () => {
     lines.forEach(item => {
-      if (item.classList.value.indexOf('show-line') > -1) {
-        item.classList.remove('show-line');
+      if (item.classList.value.indexOf("show-line") > -1) {
+        item.classList.remove("show-line");
       }
     });
-    link.getElementsByTagName('hr')[0].classList.add('show-line');
+    screens.forEach(item => {
+      if (item.id != link.getElementsByTagName("a")[0].innerHTML) {
+        item.style.display = "none";
+      } else {
+        item.style.display = "block";
+      }
+    });
+    link.getElementsByTagName("hr")[0].classList.add("show-line");
   });
 });
